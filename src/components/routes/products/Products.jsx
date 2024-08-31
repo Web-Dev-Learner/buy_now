c2f93dd2cdf3ae6eb034112c5c5539e3f0fb35e0
Web-Dev-Learner <shribabe2@gmail.com>
2024-08-31 17:08:31 +0530

    updated products

--- src/components/routes/products/Products.jsx
+++ src/components/routes/products/Products.jsx
@@ -1,5 +1,3 @@
-// Packages
-import { motion, AnimatePresence } from "framer-motion";
 import { Outlet } from "react-router-dom";
 // Components
 import FilterCategories from "./filterCategories/FilterCategories";
@@ -8,22 +6,14 @@ import styles from "./Products.module.css";
 
 export default function Products() {
     return (
-        <AnimatePresence>
-            <motion.section
-                key="products"
-                className={styles.section}
-                aria-label="Products section"
-                initial={{
-                    opacity: 0,
-                }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                exit={{ x: "-100%" }}
-            >
-                <FilterCategories />
-                <div className={styles.divider}></div>
-                <Outlet />
-            </motion.section>
-        </AnimatePresence>
+        <section
+            key="products"
+            className={styles.section}
+            aria-label="Products section"
+        >
+            <FilterCategories />
+            <div className={styles.divider}></div>
+            <Outlet />
+        </section>
     );
 }
--- src/components/routes/products/singleProduct/productNotFoundl/productNotFound.jsx
+++ src/components/routes/products/singleProduct/productNotFoundl/productNotFound.jsx
@@ -1,17 +1,10 @@
 import styles from "./ProductNotFound.module.css";
-import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import productNotFound from "../../../../../assets/images/product-not-found.svg";
 
 export default function ProductNotFound() {
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
-            className={styles.productNotFound}
-        >
+        <div className={styles.productNotFound}>
             <h1>
                 Sorry, we couldn&apos;t find the product you were looking for...
             </h1>
@@ -20,6 +13,6 @@ export default function ProductNotFound() {
                 alt="Product not found"
             />
             <Link to="/products">Go back to products</Link>
-        </motion.div>
+        </div>
     );
 }
