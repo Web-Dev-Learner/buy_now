188e6b6c9eb9ba4927a32a102242d5d3b4710716
Web-Dev-Learner <shribabe2@gmail.com>
2024-09-05 16:02:17 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/joyful-mart/",
+  base: "/joyful-mart/", 
   plugins: [react()],
-  test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "src/tests/setup.js",
-  },
+  
 })
+
