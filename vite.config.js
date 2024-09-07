d1d49cbed740ad889c66015f30b444095ec03dd3
Web-Dev-Learner <shribabe2@gmail.com>
2024-09-07 09:57:25 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
+/* import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  base: "/buy-now/", // Base path for GitHub Pages deployment
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Alias for 'src' directory
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fakestoreapi.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
+*/
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: "/buy_now/", // Ensure this matches the GitHub Pages URL path
+  base: "/", // Use "/" for Vercel deployment
   plugins: [react()],
   resolve: {
     alias: {
