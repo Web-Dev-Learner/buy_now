98e3231f1c097c0e7956e573c33c551b98df0dff
Web-Dev-Learner <shribabe2@gmail.com>
2024-09-07 09:32:39 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: "/buy-now/",
+  base: "/buy_now/", // Ensure this matches the GitHub Pages URL path
   plugins: [react()],
   resolve: {
     alias: {
